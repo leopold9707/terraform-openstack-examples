007395ad24c27a81f101a7239b496beb394f0b63
leopold9707 <rkqcjs123@gmail.com>
2020-06-13 21:28:11 +0900

    01 edit

--- 01-sample-instance/network.tf
+++ 01-sample-instance/network.tf
@@ -1,14 +1,14 @@
 #### NETWORK CONFIGURATION ####
 
 # Network creation
-resource "openstack_networking_network_v2" "vpc-1" {
+resource "openstack_networking_network_v2" "vpc" {
   name = "private-net-1"
 }
 
 # Subnet http configuration
 resource "openstack_networking_subnet_v2" "http" {
   name            = var.network_http["subnet_name"]
-  network_id      = openstack_networking_network_v2.vpc-1.id
+  network_id      = openstack_networking_network_v2.vpc.id
   cidr            = var.network_http["cidr"]
   dns_nameservers = var.dns_ip
 }
