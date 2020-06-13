5f7bf053b29c877e77c223176dab5f2cc399d415
leopold9707 <rkqcjs123@gmail.com>
2020-06-13 14:21:44 +0900

    edit 01-sample-instance

--- 01-sample-instance/00-variables.tf
+++ 01-sample-instance/00-variables.tf
@@ -3,19 +3,19 @@
 #### GLANCE
 variable "image" {
   type    = string
-  default = "Centos 7"
+  default = "CentOS-7-1907"
 }
 
 #### NEUTRON
 variable "external_network" {
   type    = string
-  default = "external-network"
+  default = "public-provider-97"
 }
 
 # UUID of external gateway
 variable "external_gateway" {
   type    = string
-  default = "f67f0d72-0ddf-11e4-9d95-e1f29f417e2f"
+  default = "e98e5ab1-1775-46bb-8a7b-ae1ced2950ff"
 }
 
 variable "dns_ip" {
@@ -26,14 +26,14 @@ variable "dns_ip" {
 #### VM parameters
 variable "flavor_http" {
   type    = string
-  default = "t2.medium"
+  default = "m1.medium"
 }
 
 variable "network_http" {
   type    = map(string)
   default = {
     subnet_name = "subnet-http"
-    cidr        = "192.168.1.0/24"
+    cidr        = "11.11.11.0/24"
   }
 }
 
