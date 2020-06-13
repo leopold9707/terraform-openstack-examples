b3bfd7ef4f4cdfc391a35ecc6a5e16b85ecd4227
leopold9707 <rkqcjs123@gmail.com>
2020-06-13 18:21:23 +0900

    중복되어 생성할 필요 없는 키페어와 라우터와 보안그룹을 구성해주는 디렉토리

---
+++ 00-key-and-router/init.tf
@@ -0,0 +1,62 @@
+### Data Source ###
+
+# 사용 가능한 외부망 네트워크
+data "openstack_networking_network_v2" "external_gateway" {
+  name = "public-provider-97" ##FIXME
+}
+
+### Variable ###
+
+# 사용할 DNS
+variable "dns_ip" {
+  type    = list(string)
+  default = ["8.8.8.8", "8.8.8.4"] ##FIXME
+}
+
+### Resource ###
+
+# 사용할 키페어
+resource "openstack_compute_keypair_v2" "user_key" {
+  name       = "jinho_key"
+  public_key = file("~/.ssh/id_rsa.pub") ##FIXME
+}
+
+# Router 생성
+resource "openstack_networking_router_v2" "router" {
+  name                = "jinho-router"
+  external_network_id = data.openstack_networking_network_v2.external_gateway.id
+}
+
+# 3가지 보안그룹 생성
+resource "openstack_compute_secgroup_v2" "http" {
+  name        = "http"
+  description = "Open input http port"
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "ssh" {
+  name        = "ssh"
+  description = "Open input ssh port"
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "db" {
+  name        = "db"
+  description = "Open input db port"
+  rule {
+    from_port   = 3306
+    to_port     = 3306
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
