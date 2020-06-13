df939dcb556b7ebe1bc30271d5d7f6598da86458
leopold9707 <rkqcjs123@gmail.com>
2020-06-13 15:12:38 +0900

    01이 생성한 라우터를 사용하도록 02 소스코드 수정

--- 02-instance-with-volume/020-network.tf
+++ 02-instance-with-volume/020-network.tf
@@ -1,10 +1,16 @@
 #### NETWORK CONFIGURATION ####
 
-# Router creation
-resource "openstack_networking_router_v2" "generic" {
-  name                = "router"
-  external_network_id = var.external_gateway
+## Router creation
+#resource "openstack_networking_router_v2" "generic" {
+#  name                = "router-generic-2"
+#  external_network_id = var.external_gateway
+#}
+##기존 라우터를 사용하면 되기에, 새로 생성하지 않을 것임.
+
+data "openstack_networking_router_v2" "generic" {
+  name = "router-generic"
 }
+
 
 # Network creation
 resource "openstack_networking_network_v2" "generic" {
@@ -23,7 +29,7 @@ resource "openstack_networking_subnet_v2" "http" {
 
 # Router interface configuration
 resource "openstack_networking_router_interface_v2" "http" {
-  router_id = openstack_networking_router_v2.generic.id
+  router_id = "79ec61ef-e15b-4ed6-8676-0d09afb95fab"
   subnet_id = openstack_networking_subnet_v2.http.id
 }
 
